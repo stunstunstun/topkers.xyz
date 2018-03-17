1e7c39d5b57109387fa713772c1aa5a578b5571c
stunstunstun <wjdsupj@gmail.com>
2018-03-18 02:16:36 +0900

    Fixed some lint errors

--- packages/blahblah-app/src/reducers/reddit.js
+++ packages/blahblah-app/src/reducers/reddit.js
@@ -1,6 +1,6 @@
 import { REQUEST_POSTS, RECEIVE_POSTS } from '../actions/reddit'
 
-function reddit(state, action) {
+function reddit(state = {}, action) {
   switch (action.type) {
     case REQUEST_POSTS:
       return {
